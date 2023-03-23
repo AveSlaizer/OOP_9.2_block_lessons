37347f282b194054890fefaacdbf51d1659764ae
AS. Sushkov <aveslaizer@gmial.com>
2023-03-23 20:39:52 +0300

    Добавил методы в класс Triangle

--- figures.py
+++ figures.py
@@ -1,5 +1,24 @@
+from math import sqrt
+
+
 class Triangle:
-    pass
+
+    def __init__(self, a: float, b: float, c: float):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+
+    def height(self, key: str):
+        side = {"a": self.__a, "b": self.__b, "c": self.__c}
+        p = 0.5 * (self.__a + self.__b + self.__c)
+        return 2 * sqrt(p * (p - self.__a) * (p - self.__b) * (p - self.__c)) / side[key]
+
+    @staticmethod
+    def is_triangle(a: float, b: float, c: float):
+        if a < b + c and b < a + c and c < a + b:
+            return Triangle(a, b, c)
+        else:
+            raise Exception("Треугольник с данными торонами не может существовать.")
 
 
 class Circle:
