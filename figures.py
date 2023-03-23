39395281a0a38fc1a36f893e128b0ce52f8e832e
AS. Sushkov <aveslaizer@gmial.com>
2023-03-23 21:12:22 +0300

    Добавил геттеры и сеттеры классу Triangle

--- figures.py
+++ figures.py
@@ -8,6 +8,18 @@ class Triangle:
         self.__b = b
         self.__c = c
 
+    @property
+    def a(self):
+        return self.__a
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def c(self):
+        return self.__c
+
     def height(self, key: str):
         side = {"a": self.__a, "b": self.__b, "c": self.__c}
         p = 0.5 * (self.__a + self.__b + self.__c)
@@ -18,7 +30,15 @@ class Triangle:
         if a < b + c and b < a + c and c < a + b:
             return Triangle(a, b, c)
         else:
-            raise Exception("Треугольник с данными торонами не может существовать.")
+            raise Exception("Треугольник с данными cторонами не может существовать.")
+
+    @classmethod
+    def create_triangle_by_angles(cls, a: float, angle_a: int, angle_b: int):
+
+    def __str__(self):
+        return f"Сторона А: {self.__a}\n" \
+               f"Сторона В: {self.__b}\n" \
+               f"Сторона С: {self.__c}\n"
 
 
 class Circle:
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from geometry_square import GeometrySquare
+from figures import Triangle
+
 
 def execute_application():
-    pass
+    a = float(input("Введите сторону треугольника А: "))
+    b = float(input("Введите сторону треугольника В: "))
+    c = float(input("Введите сторону треугольника С: "))
+    try:
+        triangle = Triangle.is_triangle(a, b, c)
+        triangle.h_a = triangle.height("a")
+        triangle.square = GeometrySquare.triangle_square_1(triangle.a, triangle.h_a)
+        print(triangle)
+        print(f"Площадь: {triangle.square}")
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
