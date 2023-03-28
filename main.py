a3b9534464da885c4f35fb224dd3a17d6ea49b2d
AS. Sushkov <aveslaizer@gmial.com>
2023-03-28 20:11:01 +0300

    Написал несколько классов

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+"""
+Наследование.
+1. Позволяет выделить общие характеристики для нескольких классов
+2. Позволяет избежать повторения кода при определении классов
+3. Один из принципов ООП
+"""
+
+
+class Person:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+
+class Programmer:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+
+class Employee:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
 
 
 def execute_application():
