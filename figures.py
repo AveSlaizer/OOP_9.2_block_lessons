37f187d1eb117d3b608188d85182f5aaf01c39b3
AS. Sushkov <aveslaizer@gmial.com>
2023-04-04 20:51:40 +0300

    Коммит на зло Юре

--- figures.py
+++ figures.py
@@ -1,4 +1,4 @@
-from math import pi
+from math import pi, sin, sqrt
 from abc import ABC, abstractmethod
 
 
@@ -6,6 +6,16 @@ class Figure:
     def __init__(self, x: int, y: int):
         self.__x = x
         self.__y = y
+
+    @property
+    #@abstractmethod
+    def x(self):
+        return self.__x
+
+    @x.setter
+    #@abstractmethod
+    def x(self, x: int):
+        self.__x = x
 
     @abstractmethod
     def area(self):
@@ -16,18 +26,44 @@ class Figure:
         pass
 
 
+class RectangleFormulas(ABC):
+
+    @abstractmethod
+    def area_by_diagonal(self):
+        pass
+
+
+class CircleFormulas(ABC):
+    pass
+
+
 # Прямоугольник
-class Rectangle(Figure):
+class Rectangle(Figure, RectangleFormulas):
     def __init__(self, x: int, y: int, width: int, height: int):
         super().__init__(x, y)
         self.__width = width
         self.__height = height
 
-    #def area(self):
-     #   return self.__width * self.__height
+    @property
+    def diagonal(self):
+        return self.diagonal
+
+    @diagonal.setter
+    def diagonal(self, d):
+        self.diagonal = d
+
+    def area(self):
+        return self.__width * self.__height
 
     def perimetr(self):
         return 2 * (self.__width + self.__height)
+
+    def __diagonal(self):
+        return sqrt(self.__width ** 2 + self.__height ** 2)
+
+    def area_by_diagonal(self):
+        d = self.__diagonal()
+        return self.__width * sqrt(d ** 2 - self.__width ** 2)
 
 
 # Окружность
