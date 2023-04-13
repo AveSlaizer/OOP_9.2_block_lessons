871c6555b41372efcd0f9d398b1024f5d0a68cbe
AS. Sushkov <aveslaizer@gmial.com>
2023-04-13 20:08:54 +0300

    Переопределил методы сравнения класса Time

--- Time_class.py
+++ Time_class.py
@@ -14,6 +14,11 @@ class Time:
         # TODO: реализовать метод
         pass
 
+    def __is_time(self, other):
+        if not isinstance(other, Time):
+            raise TypeError(f"Невозможно выполнить сравнение между типами {self.__class__.__name__} "
+                            f"и {other.__class__.__name__}")
+
     def __str__(self):
         hours = self.__value // Time.HOUR % 24
         minute = self.__value // Time.MINUTE % 60
@@ -21,3 +26,40 @@ class Time:
         return f"{Time.__format(hours)}:" \
                f"{Time.__format(minute)}:" \
                f"{Time.__format(second)}"
+
+    def __eq__(self, other):
+        self.__is_time(other)
+        return self.__value == other.__value
+
+    def __lt__(self, other):
+        self.__is_time(other)
+        return self.__value < other.__value
+
+    def __le__(self, other):
+        self.__is_time(other)
+        return self.__value <= other.__value
+
+    def __ne__(self, other):
+        self.__is_time(other)
+        return self.__value != other.__value
+
+    def __gt__(self, other):
+        self.__is_time(other)
+        return self.__value > other.__value
+
+    def __ge__(self, other):
+        self.__is_time(other)
+        return self.__value >= other.__value
+
+    def __hash__(self):
+        return hash(self.__value)
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            t = self.__value + other
+            return Time(t)
+        if isinstance(other, Time):
+            t = self.__value + other.__value
+            return Time(t)
+        raise TypeError(f"Невозможно выполнить операцию сложения между типами {self.__class__.__name__} и "
+                        f"{other.__class__.__name__}")
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from Time_class import Time
 
 def execute_application():
     time = Time(91)
-
+    time2 = Time(91)
     print(time)
+    print(time == time2)
 
 
 if __name__ == "__main__":
